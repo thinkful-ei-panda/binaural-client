7a3e33662b50660791e2142a10fe07bbee567f5b
Wesley Rou <wesleyrou@gmail.com>
2020-09-10 10:13:43 -0700

    more experiments

--- src/routes/ThingyRoute/ThingyRoute.js
+++ src/routes/ThingyRoute/ThingyRoute.js
@@ -1,37 +1,25 @@
 import React, { Component } from 'react'
-import * as THREE from 'three';
+// import * as THREE from 'three';
+// import SimplexNoise from 'simplex-noise'
+import {Tone} from 'tone'
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+const handleButtonClick = () => {
+  //create a synth and connect it to the main output (your speakers)
+  const synth = new Tone.Synth().toDestination();
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+  //play a middle 'C' for the duration of an 8th note
+  synth.triggerAttackRelease("C4", "8n");
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-var animate = function () {
-	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
-};
-
-
-
+}
 class ThingyRoute extends Component {
+    
   
-  render() {
+  render() {    
+
     return (
-      <section>
-        {animate()}
+      <section>     
+        <button onClick = {handleButtonClick}>Play</button>   
+
       </section>
     );
   }
