fb740a603e5ec899f63c5d1f7c10d424efd41831
seraphyne <renatafd717@gmail.com>
2020-09-14 16:58:38 -0500

    changes made on config, env and cypressjson. Cypress running. No tests passing yet

--- src/config.js
+++ src/config.js
@@ -1,4 +1,12 @@
-export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'blogful-client-auth-token',
+let BASE_URL;
+
+if(process.env.NODE_ENV === 'development'){
+  BASE_URL='http://localhost:8000';
 }
+else if(process.env.NODE_ENV === 'production'){
+  BASE_URL=process.env.REACT_APP_BASE_URL
+}
+
+export default BASE_URL;
+export const API_KEY=process.env.REACT_APP_BASE_URL;
+export const TOKEN_KEY = 'binaural-token';
--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './contexts/UserContext'
 import App from './components/App/App'
-import './setup-icons'
 import './index.css'
-import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <BrowserRouter>
@@ -16,5 +14,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root'),
 )
-
-serviceWorker.unregister()
