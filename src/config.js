87ade9320d575bfe2cf62ef51e2c87dfb5e5c63a
Karsten Taylor <karstentaylor0@gmail.com>
2020-09-14 13:38:32 -0700

    merged

--- src/config.js
+++ src/config.js
@@ -1,9 +1,4 @@
 export default {
-<<<<<<< HEAD
-  API_ENDPOINT: "http://localhost:8000",
-  TOKEN_KEY: "blogful-client-auth-token",
-=======
 	API_ENDPOINT: 'http://localhost:8000',
 	TOKEN_KEY: 'blogful-client-auth-token',
->>>>>>> dev
 };
