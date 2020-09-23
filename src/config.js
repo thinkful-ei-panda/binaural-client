8c1a39faa40a2311c2e7ba0eecddb78a4259dae6
Karsten Taylor <karstentaylor0@gmail.com>
2020-09-23 09:49:07 -0700

    api endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: process.env.API_ENDPOINT,
-  TOKEN_KEY: process.env.REACT_APP_API_KEY,
+	API_ENDPOINT: 'https://intense-mesa-95345.herokuapp.com',
+	TOKEN_KEY: '0ddcb275-94a0-4222-94d3-c7ceadef8c6f',
 };
