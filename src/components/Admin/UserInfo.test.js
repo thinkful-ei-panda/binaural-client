baf8f0fa4c3dc33c5829e9bc78410dc7c60691b4
seraphyne <renatafd717@gmail.com>
2020-09-23 16:23:03 -0500

    UserInfo.test fixed. 28 tests passing

--- src/components/Admin/UserInfo.test.js
+++ src/components/Admin/UserInfo.test.js
@@ -4,13 +4,18 @@ import { mount, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import UserInfo from "./UserInfo";
 
-
 configure({adapter: new Adapter()});
 describe('<UserInfo />', () => {
   it('renders without crashing', () => {
+    const user = {
+      name: "Test",
+      email: "email@email.com",
+      id: 1,
+      create_date: new Date()
+    }
     mount(
       <BrowserRouter>
-        <UserInfo />
+        <UserInfo user={user}/>
       </BrowserRouter>,
     );
   });
